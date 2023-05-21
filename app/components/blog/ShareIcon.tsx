11d518d70fdec2977ba3db5dc24e053d051fabf8
lexjames06 <ajs.stewart@btinternet.com>
2023-05-22 00:11:34 +0100

    update social links

--- app/components/blog/ShareIcon.tsx
+++ app/components/blog/ShareIcon.tsx
@@ -1,68 +1,79 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { AiFillTwitterSquare, AiFillFacebook, AiFillLinkedin } from "react-icons/ai";
 import { MdShare } from "react-icons/md";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export type SocialPlatform = "twitter" | "facebook" | "linkedIn" | "share";
 
 type Props = {
-  platform: SocialPlatform;
-  title: string;
-}
+	platform: SocialPlatform;
+	title: string;
+};
 
 export default function ShareIcon({ platform, title }: Props) {
-  const router = useRouter();
-  const [location, setLocation] = useState<Location>();
+	const [location, setLocation] = useState<Location>();
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setLocation(window.location);
-    }
-  }, []);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setLocation(window.location);
+		}
+	}, []);
 
-  if (!location || (platform === "share" && !navigator?.share)) {
-    return null;
-  }
+	if (!location || (platform === "share" && !navigator?.share)) {
+		return null;
+	}
 
-  const share = async (platform: string) => {
-    switch(platform) {
-      case "twitter":
-        return router.push(`http://twitter.com/share?text=${encodeURIComponent(title ?? "")}&url=${location?.href}&hashtags=AHouseOfIdeas`);
-      case "facebook":
-        return router.push(`https://www.facebook.com/sharer/sharer.php?u=${location?.href}`);
-      case "reddit":
-        return router.push(`https://www.reddit.com/submit?url=${location?.href}&title=${encodeURIComponent(title ?? "")}&selftext=true`);
-      case "share":
-        await navigator.share({
-          title: title,
-          text: title,
-          url: location?.href,
-        }).then(() => console.log("success"))
-          .catch((err) => console.log(err));
-      default:
-        return null;
-    }
-  }
+	const getLink = (platform: string): string => {
+		switch (platform) {
+			case "twitter":
+				return `http://twitter.com/share?text=${encodeURIComponent(title ?? "")}&url=${
+					location?.href
+				}&hashtags=AHouseOfIdeas`;
+			case "facebook":
+				return `https://www.facebook.com/sharer/sharer.php?u=${location?.href}`;
+			case "reddit":
+				return `https://www.reddit.com/submit?url=${location?.href}&title=${encodeURIComponent(
+					title ?? ""
+				)}&selftext=true`;
+			default:
+				return "";
+		}
+	};
 
-  const getButton = (platform: string) => {
-    switch (platform) {
-      case "facebook":
-        return <AiFillFacebook color="#4267B2" />;
-      case "linkedIn":
-        return <AiFillLinkedin color="#0072b1" />;
-      case "twitter":
-        return <AiFillTwitterSquare color="#00acee" />;
-      case "share":
-        return <MdShare />;
-      default:
-        return null;
-    }
-  };
+	const share = async () => {
+		await navigator
+			.share({
+				title: title,
+				text: title,
+				url: location?.href,
+			})
+			.then(() => console.log("success"))
+			.catch((err) => console.log(err));
+	};
 
-  return (
-    <button onClick={() => share(platform)}>
-      {getButton(platform)}
-    </button>
-  );
+	const getIcon = (platform: string) => {
+		switch (platform) {
+			case "facebook":
+				return <AiFillFacebook color="#4267B2" />;
+			case "linkedIn":
+				return <AiFillLinkedin color="#0072b1" />;
+			case "twitter":
+				return <AiFillTwitterSquare color="#00acee" />;
+			case "share":
+				return <MdShare />;
+			default:
+				return null;
+		}
+	};
+
+	if (platform === "share") {
+		return <button onClick={() => share()}>{getIcon(platform)}</button>;
+	}
+
+	return (
+		<Link href={getLink(platform)} target="_blank">
+			{getIcon(platform)}
+		</Link>
+	);
 }
