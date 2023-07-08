b315e5b9a3b66ae41f71cc3e7f98f3b8ca6c248a
lexjames06 <ajs.stewart@btinternet.com>
2023-07-08 23:42:53 +0200

    update not found page

--- app/posts/[path]/not-found.tsx
+++ app/posts/[path]/not-found.tsx
@@ -1,5 +1,18 @@
+"use client";
+import Page from "@/app/components/Page";
+import { useRouter } from "next/navigation";
 import React from "react";
+import styles from "./not-found.module.scss";
 
 export default function NotFound() {
-	return <div>not-found</div>;
+	const router = useRouter();
+
+	return (
+		<Page className={styles.container}>
+			<div className={styles.title}>The post you are looking for does not exist</div>
+			<button className={styles.button} onClick={() => router.push("/posts")}>
+				Go to Blog
+			</button>
+		</Page>
+	);
 }
