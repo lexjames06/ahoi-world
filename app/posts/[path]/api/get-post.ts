697d80d19539ce9c089ee295cf9ccd589af2902c
lexjames06 <ajs.stewart@btinternet.com>
2023-07-08 18:25:30 +0200

    update og image api

--- app/posts/[path]/api/get-post.ts
+++ app/posts/[path]/api/get-post.ts
@@ -2,9 +2,9 @@ import { getPostData } from "@/lib/posts";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function getPost(req: NextApiRequest, res: NextApiResponse<{ image: string, title: string }>) {
-  let { postId } = req.query as { postId: string };
+  let { path } = req.query as { path: string };
 
-  const { image, title } = await getPostData(postId);
+  const { image, title } = await getPostData(path);
 
   res.status(200);
   res.json({ image, title });
